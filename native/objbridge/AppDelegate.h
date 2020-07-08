ed2abfcae6e1aa26275a4ec3655f884469312aba
Phodal Huang <h@phodal.com>
2020-07-08 13:01:30 +0800

    feat: make example for bridge run

---
+++ native/objbridge/AppDelegate.h
@@ -0,0 +1,12 @@
+#import <AppKit/AppKit.h>
+#import <Foundation/Foundation.h>
+
+#include "bridge.h"
+
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification;
+
+@end
---
+++ native/objbridge/bridge.h
@@ -0,0 +1,12 @@
+//
+// Created by Fengda Huang  on 2020/7/8.
+//
+
+#ifndef TYPING_BRIDGE_H
+#define TYPING_BRIDGE_H
+
+extern "C" {
+void open_settings_panel();
+};
+
+#endif //TYPING_BRIDGE_H
