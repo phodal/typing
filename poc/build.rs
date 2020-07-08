7a32930311b3c45d46445e4f60af259d240fce6a
Phodal Huang <h@phodal.com>
2020-07-08 13:18:46 +0800

    build: try to add buildfile

---
+++ poc/build.rs
@@ -0,0 +1,25 @@
+extern crate cmake;
+
+use cmake::Config;
+use std::path::PathBuf;
+
+
+#[cfg(target_os = "macos")]
+fn get_config() -> PathBuf {
+    Config::new("/Users/fdhuang/repractise/typing/native/objbridge").build()
+}
+
+#[cfg(target_os = "macos")]
+fn print_config() {
+    println!("cargo:rustc-link-lib=dylib=c++");
+    println!("cargo:rustc-link-lib=static=libobjbridge");
+    println!("cargo:rustc-link-search=native=/Users/fdhuang/repractise/typing/native/objbridge");
+    println!("cargo:rustc-link-lib=framework=Cocoa");
+    println!("cargo:rustc-link-lib=framework=IOKit");
+}
+
+fn main() {
+    let path = get_config();
+    println!("cargo:rustc-link-search=native={}", path.display());
+    print_config();
+}
--- poc/src/main.rs
+++ poc/src/main.rs
@@ -1,8 +1,3 @@
-// main.rs
-extern {
-    fn doubler(x: u32) -> u32;
-}
-
 fn main() {
-    unsafe { println!("{}", doubler(1)); }
+    println!("hello, world!");
 }
